943c79bc13a549365f6d9603d2d1e52012d28ba8
denizay <denizay333@gmail.com>
2025-02-24 13:53:13 +0900

    feat: add more test cases for sql match and improve sql match to handle all test cases

--- src/txt2sql/metrics/sql_match.py
+++ src/txt2sql/metrics/sql_match.py
@@ -2,18 +2,26 @@ import io
 from contextlib import redirect_stdout
 
 import sqlfluff
+import sqlparse
 
 
 def normalize_query(query: str, dialect: str) -> str:
-    query = " ".join(query.split())
+    """Normalize spacing, casing, identation etc. and remove comments from a SQL query"""
     try:
         f = io.StringIO()
         with redirect_stdout(f):
             formatted_query = sqlfluff.fix(query, dialect=dialect)
+        formatted_query = sqlparse.format(
+            formatted_query,
+            strip_comments=True,
+            keyword_case="upper",
+            reindent=True,
+        )
         return formatted_query
     except:
         return query
 
 
 def sql_match(pred_sql: str, label_sql: str, dialect: str = "sqlite") -> bool:
+    """Normalize two queries and check if they are the same query"""
     return normalize_query(pred_sql, dialect) == normalize_query(label_sql, dialect)
